f9cc97b88607c52aa0ccd6b3980fa319579ca675
igrief <igrief2@uic.edu>
2019-03-19 15:11:08 -0500

    Commit unity project with .gitignore properly
    Sorry I set up the project incorrectly, accidentally pushed all my library files and stuff because the gitignore was in the wrong place. This should rectify those issues.

---
+++ Gun Jumpers/Assets/Damageable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+	public float health = 50f;
+
+	public void Damage(float damage){
+		health -= damage;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(health <= 0f){
+			Destroy(this.gameObject); //destroy object or do some other behavior
+		}
+    }
+}
---
+++ Gun Jumpers/Assets/GunScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+	[SerializeField] private GameObject player;
+	[SerializeField] private GameObject bullet;
+	[SerializeField] private GameObject bulletSpawner;
+	public float bulletVeloc = 30;
+	public float knockbackVeloc = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetButtonDown ("Fire1")) {
+			GameObject newBullet = GameObject.Instantiate (bullet, bulletSpawner.transform.position, bulletSpawner.transform.rotation) as GameObject;
+			newBullet.GetComponent<Rigidbody>().velocity += transform.forward * bulletVeloc;
+			player.GetComponent<Rigidbody>().velocity -= transform.forward * knockbackVeloc; 
+		}
+    }
+}
