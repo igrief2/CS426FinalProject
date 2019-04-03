c1bbd4fc120c722ee6f9bb9ec16c2146d503ebf8
igrief <igrief2@uic.edu>
2019-04-02 23:02:51 -0500

    added jump pads and sounds

---
+++ Gun Jumpers/Assets/JumpPad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+	public AudioSource woosh;
+	public float jumpVelocity = 50f; 
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			other.GetComponent<Rigidbody>().velocity += this.transform.up * jumpVelocity;
+			woosh.Play();
+		}
+	}
+}
