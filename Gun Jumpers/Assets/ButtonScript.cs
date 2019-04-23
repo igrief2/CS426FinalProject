ebcf94090031f76d90c8e51ade750fb4fbb3eae1
igrief <igrief2@uic.edu>
2019-04-23 11:38:01 -0500

    working volume slider, button sounds

---
+++ Gun Jumpers/Assets/ButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+	public AudioSource hover;
+	public AudioSource press;
+
+	public void PlayHover(){
+		hover.Play();
+	}
+	public void PlayPress(){
+		press.Play();
+	}
+}
