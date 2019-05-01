a7af6795a9d894c586b91e4e95a3a9bab7cddc87
Josh Peterson <jpeterson1352@gmail.com>
2019-05-01 02:42:45 -0500

    Added Controls to main menu Deleted Empty Script

--- Gun Jumpers/Assets/Scripts/MainMenu.cs
+++ Gun Jumpers/Assets/Scripts/MainMenu.cs
@@ -6,7 +6,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 	public void QuitGame(){
-		Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
 	}
 
 	public void PlayMultiplayer(){
